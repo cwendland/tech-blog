551137c109c17a887a838bfef207c0295facec82
cwendland <chris.r.wendland@gmail.com>
2022-10-31 10:26:41 -0700

    Added blog and comment models, also set up the relations between all models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,22 @@
 const User = require('./User');
+const Comment = require('./Comment');
+const Blog = require('./Blog');
+
+Blog.belongsTo(User, {
+  foreignKey: 'creator_id',
+});
+
+User.hasMany(Blog, {
+  foreignKey: 'creator_id',
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id',
+});
 
 
-module.exports = { User };
+module.exports = { User, Blog, Comment };
